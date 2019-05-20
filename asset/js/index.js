5c6ab3aeacb3daac2f7ce0faa4ccd9b33465ebcd
Ali-Janan <janan.torkman@gmail.com>
2019-05-21 00:14:42 +0200

    Shopping Cart plugin with jQuery and Bootrap is added to the site

--- asset/js/index.js
+++ asset/js/index.js
@@ -1,5 +1,5 @@
 /**
- * READ MORE FUNTION
+ * SHOW MORE AND SHOW LESS FUNTION
  */
 function myFunction() {
   var jan = document.getElementById("jan");
@@ -16,8 +16,10 @@ function myFunction() {
     moreText.style.display = "flex";
   }
 }
-
 //----------------------------------------------------
+/**
+ * SEARCH FUNCTION
+ */
 function searchFunction() {
   // Declare variables
   var input, filter, ul, li, a, i, txtValue;
@@ -28,9 +30,8 @@ function searchFunction() {
   
   // Loop through all list items, and hide those who don't match the search query
   for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName("a")[0];
-    p = a.getElementsByTagName("p")[0];
-    txtValue = p.textContent || a.innerText;
+    a = li[i].getElementsByClassName("name")[0];
+    txtValue =  a.innerText;
     if (txtValue.toUpperCase().indexOf(filter) > -1) {
       li[i].style.display = "";
     } else {
@@ -39,7 +40,10 @@ function searchFunction() {
     
   }
 }
-
+//------------------------------------------------------------------
+/**
+ * ADDTOBASKET FUNCTION
+ */
 function addToBasket (event) {
   console.log("hello", event);
 }
@@ -62,8 +66,88 @@ menuElement.addEventListener('click', function(e) {
 */
 
 
-
+//------------------------------------------------------
+/**
+ * JQUERY 
+ * OPEN AND CLOSE NAV WHEN CLICK
+ */
   $(".icon-menu").on('click', function() {
+    
     $(".nav").toggleClass('nav-open');
+    
 });
+$('#menu-products').on('click', function() {
+  
+  $('#aside').toggleClass('aside-open');
+});
+//------------------------------------------------------
+/**
+ * JQUERY 
+ * ADD TO BASKET
+ */
+$(document).ready(function () {
+  2
+    $('#cart').simpleCart();
+  3
+  });
 
+// -----------------------------------------------------
+$(function () {
+
+  var goToCartIcon = function($addTocartBtn){
+    var $cartIcon = $(".my-cart-icon");
+    var $image = $('<img width="30px" height="30px" src="' + $addTocartBtn.data("image") + '"/>').css({"position": "fixed", "z-index": "999"});
+    $addTocartBtn.prepend($image);
+    var position = $cartIcon.position();
+    $image.animate({
+      top: position.top,
+      left: position.left
+    }, 500 , "linear", function() {
+      $image.remove();
+    });
+  }
+
+  $('.my-cart-btn').myCart({
+    currencySymbol: '$',
+    classCartIcon: 'my-cart-icon',
+    classCartBadge: 'my-cart-badge',
+    classProductQuantity: 'my-product-quantity',
+    classProductRemove: 'my-product-remove',
+    classCheckoutCart: 'my-cart-checkout',
+    affixCartIcon: true,
+    showCheckoutModal: true,
+    numberOfDecimals: 2,
+    cartItems: [
+      {id: 1, name: 'product 1', summary: 'summary 1', price: 10, quantity: 1, image: 'images/img_1.png'},
+      {id: 2, name: 'product 2', summary: 'summary 2', price: 20, quantity: 2, image: 'images/img_2.png'},
+      {id: 3, name: 'product 3', summary: 'summary 3', price: 30, quantity: 1, image: 'images/img_3.png'}
+    ],
+    clickOnAddToCart: function($addTocart){
+      goToCartIcon($addTocart);
+    },
+    afterAddOnCart: function(products, totalPrice, totalQuantity) {
+      console.log("afterAddOnCart", products, totalPrice, totalQuantity);
+    },
+    clickOnCartIcon: function($cartIcon, products, totalPrice, totalQuantity) {
+      console.log("cart icon clicked", $cartIcon, products, totalPrice, totalQuantity);
+    },
+    checkoutCart: function(products, totalPrice, totalQuantity) {
+      var checkoutString = "Total Price: " + totalPrice + "\nTotal Quantity: " + totalQuantity;
+      checkoutString += "\n\n id \t name \t summary \t price \t quantity \t image path";
+      $.each(products, function(){
+        checkoutString += ("\n " + this.id + " \t " + this.name + " \t " + this.summary + " \t " + this.price + " \t " + this.quantity + " \t " + this.image);
+      });
+      alert(checkoutString)
+      console.log("checking out", products, totalPrice, totalQuantity);
+    },
+    getDiscountPrice: function(products, totalPrice, totalQuantity) {
+      console.log("calculating discount", products, totalPrice, totalQuantity);
+      return totalPrice * 0.5;
+    }
+  });
+
+  $("#addNewProduct").click(function(event) {
+    var currentElementNo = $(".row").children().length + 1;
+    $(".row").append('<div class="col-md-3 text-center"><img src="images/img_empty.png" width="150px" height="150px"><br>product ' + currentElementNo + ' - <strong>$' + currentElementNo + '</strong><br><button class="btn btn-danger my-cart-btn" data-id="' + currentElementNo + '" data-name="product ' + currentElementNo + '" data-summary="summary ' + currentElementNo + '" data-price="' + currentElementNo + '" data-quantity="1" data-image="images/img_empty.png">Add to Cart</button><a href="#" class="btn btn-info">Details</a></div>')
+  });
+});
